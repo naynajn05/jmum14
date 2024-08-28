4c807c27bf32d31595e6f223d7895fd8847b711b
JPMG <JPMG@LAPTOP-7CCTN912>
2024-08-28 13:42:11 +0530

    JDBC Prepared statement interface

--- JDBCApp/src/com/tnsif/statementinterface/CRUDStatementInterface.java
+++ JDBCApp/src/com/tnsif/statementinterface/CRUDStatementInterface.java
@@ -1,6 +1,7 @@
 package com.tnsif.statementinterface;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -38,6 +39,119 @@ public class CRUDStatementInterface {
 		}
 		return n;
 
+	}
+	
+	//validate
+	public static int validateStudent(int stuId) {
+		int n = 0;
+		try {
+			String query = "SELECT count(*) FROM studentdetails where if=" + stuId;
+			ResultSet rs = st.executeQuery(query);
+			if (rs.next()) {
+				if (rs.getInt(1) != 0)
+					n = 1;
+			}
+
+		} catch (SQLException e) {
+
+			System.out.println("Error...." + e.getMessage());
+		}
+		System.out.println("No such student found");
+
+		return n;
+	}
+
+	
+	//delete
+	public static boolean deleteStudent(int stuId) {
+		boolean status = false;
+
+		try {
+
+			int n = validateStudent(stuId);
+			if (n == 1) {
+				String query = "DELETE FROM studentdetails WHERE if=" + stuId;
+				st.executeUpdate(query);
+				status = true;
+			}
+
+		} catch (SQLException e) {
+
+			System.out.println("Error...." + e.getMessage());
+		}
+		return status;
+	}
+
+	//update
+	public static boolean updateStuName(int stuId, String stuName) {
+		boolean status = false;
+		try {
+
+			if (validateStudent(stuId) == 1) {
+				String query = "UPDATE studentdetails set name='" + stuName + "' WHERE if=" + stuId;
+				st.executeUpdate(query);
+				status = true;
+			} else
+
+				System.out.println("No such Student Record.....");
+
+		} catch (SQLException e) {
+
+			System.out.println("Error...." + e.getMessage());
+		}
+		return status;
+	}
+
+	//update percentage
+	public static boolean updateStuPercentage(int stuId, int per) {
+		boolean status = false;
+		try {
+
+			if (validateStudent(stuId) == 1) {
+				String query = "UPDATE studentdetails SET per=" + per + " WHERE if=" + stuId;
+				st.executeUpdate(query);
+				status = true;
+			} else
+				System.out.println("No such Student Record.....");
+
+		} catch (SQLException e) {
+
+			System.out.println("Error...." + e.getMessage());
+		}
+		return status;
+	}
+
+	//retrieve all
+	public static void showStudents() {
+		try {
+
+			String query = "SELECT * FROM studentdetails";
+			ResultSet rs = st.executeQuery(query);
+
+			
+			if (rs.next()) {
+				while (rs.next()) {
+
+					System.out.println(rs.getInt(1) + "\t" + rs.getString(2) + "\t" + rs.getInt(3));
+				}
+			} else
+				System.out.println("No Student Records.....");
+			rs.close();
+
+		} catch (SQLException e) {
+
+			System.out.println("Error...." + e.getMessage());
+		}
+	}
+
+	public void closeConnection() {
+		try {
+			st.close();
+			cn.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
--- JDBCApp/src/com/tnsif/statementinterface/Executor.java
+++ JDBCApp/src/com/tnsif/statementinterface/Executor.java
@@ -4,7 +4,19 @@ public class Executor {
 
 	public static void main(String[] args) {
 		DBUtil.getConnection();
-		CRUDStatementInterface.addStudent(3, "Udhay", 90);
+		/*
+		 * CRUDStatementInterface.addStudent(5, "Pallavi", 92);
+		 * CRUDStatementInterface.showStudents();
+		 * CRUDStatementInterface.updateStuName(3, "Mayuresh");
+		 * CRUDStatementInterface.showStudents();
+		 * CRUDStatementInterface.updateStuPercentage(3, 95);
+		 * CRUDStatementInterface.showStudents();
+		 * CRUDStatementInterface.deleteStudent(2);
+		 * CRUDStatementInterface.showStudents();
+		 */
+		CRUDStatementInterface.deleteStudent(6);
+		CRUDStatementInterface.showStudents();
+		
 	}
 
 }
